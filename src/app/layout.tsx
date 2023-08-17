ddb6e2163b51df8af1145ac0656b2876bbfaa244
Pablo Escobar <esc20936@uvg.edu.gt>
2023-08-17 15:18:11 -0600

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Importadora y distribuidora Kayros',
-  description: 'Importadora y distribuidora Kayros',
-}
+  title: "Importadora y distribuidora Kayros",
+  description: "Importadora y distribuidora Kayros",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="q7FBL_YLhEvd6lYPLyibis4HWieTsdNBKZUscz8nu8A"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
